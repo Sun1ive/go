613697178db5f405faa5dea591f3a9d2a1a48eac
Sun1ive <sunliveua@gmail.com>
2018-04-19 17:01:58 +0300

    doing better

--- main.go
+++ main.go
@@ -4,20 +4,25 @@ import (
 	"fmt"
 )
 
-
 // func main() {
 // 	var a = 3
 // 	fmt.Println("my first var in go", a)
 // }
 
-func main()  {
+func main() {
 	// указатель на область в памяти
 	// a := new(int);
 	// выводим в консоль значение из памяти
 	// fmt.Println(*a)
 
 	//
-	var a = 1;
+	var a = 1
 	// ссылка на адресс, тоже самое что и указатель созданый вручную
-	fmt.Println(&a);
+	fmt.Println(&a)
+
+	fmt.Println(myFunction(33, 22))
+}
+
+func myFunction(a, b int) (int, int) {
+	return b, a
 }
