38a9ef08f39dc01c4530ddd6638c60316b6b984e
Sun1ive <sunliveua@gmail.com>
2018-04-19 17:55:35 +0300

    doing better

--- main.go
+++ main.go
@@ -16,13 +16,50 @@ func main() {
 	// fmt.Println(*a)
 
 	//
-	var a = 1
+	// var a = 1
 	// ссылка на адресс, тоже самое что и указатель созданый вручную
-	fmt.Println(&a)
+	// fmt.Println(&a)
 
-	fmt.Println(myFunction(33, 22))
+	// fmt.Println(myFunction(33, 22))
+
+	myFunc()
 }
 
-func myFunction(a, b int) (int, int) {
-	return b, a
+// func myFunction(a, b int) (int, int) {
+// 	return b, a
+// }
+
+// 1й способ создать массив
+// func myFunc() {
+// 	var a = []string{"hello", "world"}
+// 	fmt.Println(a)
+// }
+
+// второй способ создать массив
+// func myFunc() {
+// 	var arr = make([]string, 5)
+// 	fmt.Print(arr)
+// }
+
+// добавить элементы в массив => append
+// func myFunc() {
+// 	var arr = make([]uint, 5)
+// 	arr = append(arr, 5, 6, 7, 8)
+// 	fmt.Println(arr)
+// }
+
+// for loop
+// func myFunc() {
+// 	var arr = []uint{1, 2, 3, 4, 5, 6, 7, 10}
+// 	for i := 0; i < len(arr); i++ {
+// 		fmt.Println(arr[i])
+// 	}
+// }
+
+// another iterator
+func myFunc() {
+	var arr = []uint{1, 2, 3, 4, 5, 6, 7, 10}
+	for _, value := range arr {
+		fmt.Println(value)
+	}
 }
