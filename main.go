ed471aa7c5e6a6ce0bee4df56151a7caa137ab89
Sun1ive <sunliveua@gmail.com>
2018-04-19 16:53:21 +0300

    doing better

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	fmt.Printf("my int number is %d", 12)
+
+// func main() {
+// 	var a = 3
+// 	fmt.Println("my first var in go", a)
+// }
+
+func main()  {
+	// указатель на область в памяти
+	// a := new(int);
+	// выводим в консоль значение из памяти
+	// fmt.Println(*a)
+
+	//
+	var a = 1;
+	// ссылка на адресс, тоже самое что и указатель созданый вручную
+	fmt.Println(&a);
 }
