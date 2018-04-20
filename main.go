d4014064a1f90b5a7b8a6417c9a2472f6029272d
Sun1ive <sunliveua@gmail.com>
2018-04-20 17:44:56 +0300

    doing better

--- main.go
+++ main.go
@@ -1,33 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"../../Lessons/go/test"
 )
 
-func helloWorld(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "Hello World")
-}
-
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-
-	myRouter.HandleFunc("/", helloWorld).Methods("GET")
-	myRouter.HandleFunc("/users", GetUsers).Methods("GET")
-	myRouter.HandleFunc("/user/{name}/{email}", NewUser).Methods("POST")
-	myRouter.HandleFunc("/user/{name}", DeleteUser).Methods("DELETE")
-	myRouter.HandleFunc("/user/{name}/{email}", UpdateUser).Methods("PUT")
-
-	log.Fatal(http.ListenAndServe(":8081", myRouter))
-}
-
 func main() {
-	fmt.Println("GO orm tutorial")
+	/* 	arr := []uint{1,2,3,4,5,6,7}
+	   	myarr := arr[0:2]
+	   	fmt.Println(myarr) */
 
-	InitialMigration()
-	
-	handleRequests()
+	test.Count()
+
 }
---
+++ test/index.go
@@ -0,0 +1,7 @@
+package test
+
+import "fmt"
+
+func Count() {
+	fmt.Println("Hello  there")
+}
