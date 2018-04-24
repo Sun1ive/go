b77d0a46f28376cbc169bc10b5e376896f63ea7a
Sun1ive <sunliveua@gmail.com>
2018-04-24 14:36:08 +0300

    doing better

--- main.go
+++ main.go
@@ -5,16 +5,68 @@ import (
 	"net/http"
 	"log"
 	"fmt"
+	"encoding/json"
 )
 
 var NOTYET = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Not implemented!"))
 })
 
+type Product struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Slug string `json:"slug"`
+	Description string `json:"description"`
+}
+
+var products = []Product{
+	Product{Id: 1, Name: "Hover Shooters", Slug: "hover-shooters", Description: "Shoot your way to the top on 14 different hoverboards"},
+	Product{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description: "Explore the depths of the sea in this one of a kind underwater experience"},
+	Product{Id: 3, Name: "Dinosaur Park", Slug: "dinosaur-park", Description: "Go back 65 million years in the past and ride a T-Rex"},
+	Product{Id: 4, Name: "Cars VR", Slug: "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
+	Product{Id: 5, Name: "Robin Hood", Slug: "robin-hood", Description: "Pick up the bow and arrow and master the art of archery"},
+	Product{Id: 6, Name: "Real World VR", Slug: "real-world-vr", Description: "Explore the seven wonders of the world in VR"},
+}
+
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("API is up and running"))
+})
+
+var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r* http.Request) {
+	payload, _ := json.Marshal(products)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(payload))
+})
+
+var AddFeedBackHanlder = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	var product Product
+
+	vars := mux.Vars(r)
+	slug := vars["slug"]
+
+	for _, p := range products {
+		if p.Slug == slug {
+			product = p
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if product.Slug != "" {
+		payload, _ := json.Marshal(product)
+		w.Write([]byte(payload))
+	} else {
+		w.Write([]byte("Product Not Found"))
+	}
+})
+
+
+
 func RouterHandlers(router *mux.Router)  {
-	router.Handle("/status", NOTYET).Methods("GET")
-	router.Handle("/products", NOTYET).Methods("GET")
-	router.Handle("/products/{slug}/feedback", NOTYET).Methods("POST")
+	router.Handle("/status", StatusHandler).Methods("GET")
+	router.Handle("/products", ProductsHandler).Methods("GET")
+	router.Handle("/products/{slug}/feedback", AddFeedBackHanlder).Methods("POST")
 }
 
 func main()  {
